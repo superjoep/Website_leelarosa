eec798ca03a9d8ecd1e9f756ee30e049400888c5
Joep <joepdekock@gmail.com>
2021-04-21 17:06:33 +0200

    Versie3

--- Index.php
+++ Index.php
@@ -17,9 +17,9 @@ function closeNav() {
 <div id="Home">
 <div class="flex-container2 sticky">
 <div><a><span class="navbutton" style="font-size:50px;cursor:pointer" onclick="openNav()">&#9776;</span></a></div>
-<div><a href="https://www.instagram.com/leelarosa.mp3/?hl=en"><i class="fab fa-instagram" style="margin-top 50%; margin-top: 35%; font-size: 40px;display: inline-block;"></i></a></div>
-<div><a href="#"><i class="fab fa-youtube" style="margin-top: 2%; font-size: 30px; display: inline-block;"></i></a></div>
-<div><a href="#"><i class="fab fa-spotify" style="margin-top: 2%; font-size: 35px; display: inline-block;"></i></a></div>
+<div><a href="https://www.instagram.com/leelarosa.mp3/?hl=en"><i class="fab fa-instagram" style="margin-top: 1500%; font-size: 40px;display: inline-block;"></i></a></div>
+<div><a href="#"><i class="fab fa-youtube" style="margin-top: 50%; font-size: 30px; display: inline-block;"></i></a></div>
+<div><a href="#"><i class="fab fa-spotify" style="margin-top: 50%; font-size: 35px; display: inline-block;"></i></a></div>
 </div>
 <div class="sticky2">
 <div class="header-bg">  
@@ -31,8 +31,8 @@ function closeNav() {
 
 <section>
 <div id="mySidenav" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="#Home">Home</a>
+  <a href="javascript:void(0)"  id="myBtn" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#" onclick="topFunction()">Home</a>
   <a href="#TourData">Tour Data</a>
   <a href="#Memories">Memories</a>
   <a href="#Contact">Contact</a>
@@ -85,5 +85,28 @@ function closeNav() {
         $(window).scroll(function(){
             $('.header-bg').css("opacity", 1- $(window).scrollTop() / 700)
         })
-    })</script>
+    })
+    
+    
+    //Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+    </script>
+    
 </body>
