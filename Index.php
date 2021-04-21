44a5938c54864903cf9eab557975246303ac1343
Joep <joepdekock@gmail.com>
2021-04-21 16:47:36 +0200

    Versie 2

--- Index.php
+++ Index.php
@@ -13,48 +13,44 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 </script>
-<body style="background-color: #FFFBE7">
+<body>
 <div id="Home">
- 
-  <div id="mySidenav" class="sidenav">
+<div class="flex-container2 sticky">
+<div><a><span class="navbutton" style="font-size:50px;cursor:pointer" onclick="openNav()">&#9776;</span></a></div>
+<div><a href="https://www.instagram.com/leelarosa.mp3/?hl=en"><i class="fab fa-instagram" style="margin-top 50%; margin-top: 35%; font-size: 40px;display: inline-block;"></i></a></div>
+<div><a href="#"><i class="fab fa-youtube" style="margin-top: 2%; font-size: 30px; display: inline-block;"></i></a></div>
+<div><a href="#"><i class="fab fa-spotify" style="margin-top: 2%; font-size: 35px; display: inline-block;"></i></a></div>
+</div>
+<div class="sticky2">
+<div class="header-bg">  
+<img src="img/LOGO_zwart.svg" id="logo_img">
+<button class="button button5">Contact</button>
+</div> 
+</div>
+</div>      
+
+<section>
+<div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
   <a href="#Home">Home</a>
   <a href="#TourData">Tour Data</a>
   <a href="#Memories">Memories</a>
   <a href="#Contact">Contact</a>
   </div>
- 
-<div class="flex-container2 sticky">
-<div><a><span class="navbutton" style="font-size:50px;cursor:pointer" onclick="openNav()">&#9776;</span></a></div>
-<div><a href="https://www.instagram.com/leelarosa.mp3/?hl=en"><i class="fab fa-instagram" style="margin-top 50%; padding-top: 35%; font-size: 40px;display: inline-block;"></i></a></div>
-<div><a href="#"><i class="fab fa-youtube" style="padding-top: 2%; font-size: 30px; display: inline-block;"></i></a></div>
-<div><a href="#"><i class="fab fa-spotify" style="padding-top: 2%; font-size: 35px; display: inline-block;"></i></a></div>
-</div>
-
-        
-  
-      
-   
-<div class="header-bg">   
-<img src="img/LOGO_zwart.svg" id="logo_img">
-<button class="button button5">Contact</button>
-</div>
-
-
-<section>
     <div id="axis" class="one">
         <img class="van object" src="img/Plaat.png"/>
     </div>
     <div id="axis" class="two">
-        <img class="object van move-left" style=" z-index: 0;" src="img/Plaathoes.png"/>
+        <img class="object van move-left" style="" src="img/Plaathoes.png"/>
+    </div>
     </div>
     </div>   
     <div class="flex-container5">
     <div><button class="button button6">Listen Now</button> </div>
-    <div><button class="button button6">Order Now</button> </div>
+    <div><button class="button button7">Order Now</button> </div>
     </div>
     
-   
+    
 <div id="TourData">
   <div class="tourdatatitle">Tour data</div>
   <div class="tourbox"></div>
